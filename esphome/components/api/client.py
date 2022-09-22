716f47eb38320c315b8ef4f0428d59818e944223
Gustavo Ambrozio <gustavo@gustavo.eng.br>
2022-09-22 11:57:08 -1000

    This was creating a warning. Guess we don’t need it

--- esphome/components/api/client.py
+++ esphome/components/api/client.py
@@ -13,7 +13,7 @@ from . import CONF_ENCRYPTION
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_run_logs(config, address, should_abort = None):
+async def async_run_logs(config, address, should_abort=None):
     conf = config["api"]
     port: int = int(conf[CONF_PORT])
     password: str = conf[CONF_PASSWORD]
@@ -63,7 +63,6 @@ async def async_run_logs(config, address, should_abort = None):
         while True:
             if should_abort is not None and should_abort():
                 await reconnect.stop()
-                zc.close()
                 break
             await asyncio.sleep(2)
     except KeyboardInterrupt:
@@ -71,5 +70,5 @@ async def async_run_logs(config, address, should_abort = None):
         zc.close()
 
 
-def run_logs(config, address, should_abort = None):
+def run_logs(config, address, should_abort=None):
     asyncio.run(async_run_logs(config, address, should_abort))
